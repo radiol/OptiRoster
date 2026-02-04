79b1d6b5dca55cbea2a8ff621a9bdf5c7ca7502d
radiol <radiol@github.com>
2026-02-04 19:25:28 +0900

    refactor: ruff check --fixによるimportの修正

--- src/gui/common/ensure_config.py
+++ src/gui/common/ensure_config.py
@@ -2,8 +2,9 @@
 
 from __future__ import annotations
 
+from collections.abc import Callable
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, Callable
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from src.gui.common.paths import Paths
